9d012532b897e084e21fcb39e68423d4d677478f
claudiomla <claudiomla8@gmail.com>
2016-03-08 15:25:45 -0430

    EntryVehicleTD funcionalidad
    se comenzo a crear la funcionalidad de los check
    se cambio el nombre de los objetos

---
+++ VistasDeVentanasPrincipales/VistasDeVentanasPrincipales/EntryVehicleTD.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace VistasDeVentanasPrincipales
+{
+    public partial class EntryVehicleTD : Form
+    {
+        Boolean check1=false;  //boolean para saber si el chofer esta registrado o no.
+
+        public EntryVehicleTD()
+        {
+            InitializeComponent();
+        }
+
+        private void checkTD_Click(object sender, EventArgs e)
+        {
+            
+            String CI= CITD.Text;
+            String typeCI = TypeCI.SelectedItem.ToString();
+
+            //verificar en la base de datos si se encuentra la cedula del conductor
+
+            if (/* si el nombre esta en la base de datos*/ true){
+                check1 = true;
+            }else{
+
+	        }
+        }
+
+        
+    }
+}
