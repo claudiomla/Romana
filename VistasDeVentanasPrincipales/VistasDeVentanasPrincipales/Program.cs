5e0d95f521b9b662a1270d1ccde576fbb9bf1547
claudiomla <claudiomla8@gmail.com>
2016-03-08 16:12:47 -0430

    Restauracion del proyecto
    hubo problemas con el commit entre el equipo y se dañaron varios
    archivos

--- VistasDeVentanasPrincipales/VistasDeVentanasPrincipales/Program.cs
+++ VistasDeVentanasPrincipales/VistasDeVentanasPrincipales/Program.cs
@@ -16,8 +16,7 @@ namespace VistasDeVentanasPrincipales
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            //Application.Run(new Form1());
-            Application.Run(new ingresodecodigo());
+            Application.Run(new EntryVehicleTD());
         }
     }
 }
