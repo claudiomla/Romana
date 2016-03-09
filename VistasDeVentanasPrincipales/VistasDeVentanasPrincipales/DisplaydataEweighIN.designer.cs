ec2545e7988fe61de9a4d655375349fb34dd3e1f
claudiomla <claudiomla8@gmail.com>
2016-03-09 12:09:55 -0430

    creacion de la ventana DisplayDataEwighIN
    ventana donde muestra los datos del cliente, chofer, vehiculo, e vista
    del peso de entrada del vehiculo.

---
+++ VistasDeVentanasPrincipales/VistasDeVentanasPrincipales/DisplaydataEweighIN.designer.cs
@@ -0,0 +1,389 @@
+﻿namespace VistasDeVentanasPrincipales
+{
+    partial class DisplayDataEweighIN
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.buttonback = new System.Windows.Forms.Button();
+            this.buttonnext = new System.Windows.Forms.Button();
+            this.textticket = new System.Windows.Forms.Label();
+            this.textDocument = new System.Windows.Forms.Label();
+            this.textRif = new System.Windows.Forms.Label();
+            this.textcompany = new System.Windows.Forms.Label();
+            this.textdriver = new System.Windows.Forms.Label();
+            this.textIdentity_card = new System.Windows.Forms.Label();
+            this.textplate = new System.Windows.Forms.Label();
+            this.textplatform = new System.Windows.Forms.Label();
+            this.textdate = new System.Windows.Forms.Label();
+            this.texthour = new System.Windows.Forms.Label();
+            this.texttheoretical = new System.Windows.Forms.Label();
+            this.textentry = new System.Windows.Forms.Label();
+            this.ticket = new System.Windows.Forms.TextBox();
+            this.NumDocument = new System.Windows.Forms.TextBox();
+            this.NumRif = new System.Windows.Forms.TextBox();
+            this.Company = new System.Windows.Forms.TextBox();
+            this.Driver = new System.Windows.Forms.TextBox();
+            this.IdentyCard = new System.Windows.Forms.TextBox();
+            this.Plate = new System.Windows.Forms.TextBox();
+            this.Platform = new System.Windows.Forms.TextBox();
+            this.Date = new System.Windows.Forms.TextBox();
+            this.Hour = new System.Windows.Forms.TextBox();
+            this.theorical = new System.Windows.Forms.TextBox();
+            this.Entry = new System.Windows.Forms.TextBox();
+            this.textdealings = new System.Windows.Forms.Label();
+            this.checkBoxMANUAL = new System.Windows.Forms.CheckBox();
+            this.SuspendLayout();
+            // 
+            // buttonback
+            // 
+            this.buttonback.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonback.Location = new System.Drawing.Point(154, 459);
+            this.buttonback.Name = "buttonback";
+            this.buttonback.Size = new System.Drawing.Size(107, 43);
+            this.buttonback.TabIndex = 2;
+            this.buttonback.Text = "regresar";
+            this.buttonback.UseVisualStyleBackColor = true;
+            // 
+            // buttonnext
+            // 
+            this.buttonnext.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonnext.Location = new System.Drawing.Point(367, 459);
+            this.buttonnext.Name = "buttonnext";
+            this.buttonnext.Size = new System.Drawing.Size(91, 43);
+            this.buttonnext.TabIndex = 3;
+            this.buttonnext.Text = "siguiente";
+            this.buttonnext.UseVisualStyleBackColor = true;
+            // 
+            // textticket
+            // 
+            this.textticket.AutoSize = true;
+            this.textticket.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textticket.Location = new System.Drawing.Point(32, 65);
+            this.textticket.Name = "textticket";
+            this.textticket.Size = new System.Drawing.Size(69, 18);
+            this.textticket.TabIndex = 4;
+            this.textticket.Text = "Nº Ticket";
+            // 
+            // textDocument
+            // 
+            this.textDocument.AutoSize = true;
+            this.textDocument.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textDocument.Location = new System.Drawing.Point(32, 99);
+            this.textDocument.Name = "textDocument";
+            this.textDocument.Size = new System.Drawing.Size(108, 18);
+            this.textDocument.TabIndex = 5;
+            this.textDocument.Text = "Nº Documento";
+            // 
+            // textRif
+            // 
+            this.textRif.AutoSize = true;
+            this.textRif.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textRif.Location = new System.Drawing.Point(32, 133);
+            this.textRif.Name = "textRif";
+            this.textRif.Size = new System.Drawing.Size(27, 18);
+            this.textRif.TabIndex = 6;
+            this.textRif.Text = "Rif";
+            // 
+            // textcompany
+            // 
+            this.textcompany.AutoSize = true;
+            this.textcompany.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textcompany.Location = new System.Drawing.Point(32, 174);
+            this.textcompany.Name = "textcompany";
+            this.textcompany.Size = new System.Drawing.Size(125, 18);
+            this.textcompany.TabIndex = 7;
+            this.textcompany.Text = "Empresa/Cliente";
+            // 
+            // textdriver
+            // 
+            this.textdriver.AutoSize = true;
+            this.textdriver.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textdriver.Location = new System.Drawing.Point(32, 213);
+            this.textdriver.Name = "textdriver";
+            this.textdriver.Size = new System.Drawing.Size(80, 18);
+            this.textdriver.TabIndex = 8;
+            this.textdriver.Text = "Conductor";
+            // 
+            // textIdentity_card
+            // 
+            this.textIdentity_card.AutoSize = true;
+            this.textIdentity_card.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textIdentity_card.Location = new System.Drawing.Point(367, 62);
+            this.textIdentity_card.Name = "textIdentity_card";
+            this.textIdentity_card.Size = new System.Drawing.Size(58, 18);
+            this.textIdentity_card.TabIndex = 9;
+            this.textIdentity_card.Text = "Cedula";
+            // 
+            // textplate
+            // 
+            this.textplate.AutoSize = true;
+            this.textplate.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textplate.Location = new System.Drawing.Point(367, 99);
+            this.textplate.Name = "textplate";
+            this.textplate.Size = new System.Drawing.Size(48, 18);
+            this.textplate.TabIndex = 10;
+            this.textplate.Text = "Placa";
+            // 
+            // textplatform
+            // 
+            this.textplatform.AutoSize = true;
+            this.textplatform.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textplatform.Location = new System.Drawing.Point(367, 137);
+            this.textplatform.Name = "textplatform";
+            this.textplatform.Size = new System.Drawing.Size(53, 18);
+            this.textplatform.TabIndex = 11;
+            this.textplatform.Text = "Anden";
+            // 
+            // textdate
+            // 
+            this.textdate.AutoSize = true;
+            this.textdate.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textdate.Location = new System.Drawing.Point(367, 178);
+            this.textdate.Name = "textdate";
+            this.textdate.Size = new System.Drawing.Size(52, 18);
+            this.textdate.TabIndex = 12;
+            this.textdate.Text = "Fecha";
+            // 
+            // texthour
+            // 
+            this.texthour.AutoSize = true;
+            this.texthour.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.texthour.Location = new System.Drawing.Point(367, 215);
+            this.texthour.Name = "texthour";
+            this.texthour.Size = new System.Drawing.Size(42, 18);
+            this.texthour.TabIndex = 13;
+            this.texthour.Text = "Hora";
+            // 
+            // texttheoretical
+            // 
+            this.texttheoretical.AutoSize = true;
+            this.texttheoretical.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.texttheoretical.Location = new System.Drawing.Point(164, 307);
+            this.texttheoretical.Name = "texttheoretical";
+            this.texttheoretical.Size = new System.Drawing.Size(97, 18);
+            this.texttheoretical.TabIndex = 14;
+            this.texttheoretical.Text = "Peso teorico";
+            // 
+            // textentry
+            // 
+            this.textentry.AutoSize = true;
+            this.textentry.Font = new System.Drawing.Font("Arial", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textentry.Location = new System.Drawing.Point(130, 354);
+            this.textentry.Name = "textentry";
+            this.textentry.Size = new System.Drawing.Size(161, 22);
+            this.textentry.TabIndex = 15;
+            this.textentry.Text = "Peso de entrada";
+            // 
+            // ticket
+            // 
+            this.ticket.Location = new System.Drawing.Point(185, 63);
+            this.ticket.Name = "ticket";
+            this.ticket.ReadOnly = true;
+            this.ticket.Size = new System.Drawing.Size(157, 20);
+            this.ticket.TabIndex = 16;
+            // 
+            // NumDocument
+            // 
+            this.NumDocument.Location = new System.Drawing.Point(185, 100);
+            this.NumDocument.Name = "NumDocument";
+            this.NumDocument.ReadOnly = true;
+            this.NumDocument.Size = new System.Drawing.Size(157, 20);
+            this.NumDocument.TabIndex = 17;
+            // 
+            // NumRif
+            // 
+            this.NumRif.Location = new System.Drawing.Point(185, 133);
+            this.NumRif.Name = "NumRif";
+            this.NumRif.ReadOnly = true;
+            this.NumRif.Size = new System.Drawing.Size(157, 20);
+            this.NumRif.TabIndex = 18;
+            // 
+            // Company
+            // 
+            this.Company.Location = new System.Drawing.Point(185, 171);
+            this.Company.Name = "Company";
+            this.Company.ReadOnly = true;
+            this.Company.Size = new System.Drawing.Size(157, 20);
+            this.Company.TabIndex = 19;
+            // 
+            // Driver
+            // 
+            this.Driver.Location = new System.Drawing.Point(185, 213);
+            this.Driver.Name = "Driver";
+            this.Driver.ReadOnly = true;
+            this.Driver.Size = new System.Drawing.Size(157, 20);
+            this.Driver.TabIndex = 20;
+            // 
+            // IdentyCard
+            // 
+            this.IdentyCard.Location = new System.Drawing.Point(457, 60);
+            this.IdentyCard.Name = "IdentyCard";
+            this.IdentyCard.ReadOnly = true;
+            this.IdentyCard.Size = new System.Drawing.Size(157, 20);
+            this.IdentyCard.TabIndex = 21;
+            // 
+            // Plate
+            // 
+            this.Plate.Location = new System.Drawing.Point(457, 97);
+            this.Plate.Name = "Plate";
+            this.Plate.ReadOnly = true;
+            this.Plate.Size = new System.Drawing.Size(157, 20);
+            this.Plate.TabIndex = 22;
+            // 
+            // Platform
+            // 
+            this.Platform.Location = new System.Drawing.Point(457, 137);
+            this.Platform.Name = "Platform";
+            this.Platform.ReadOnly = true;
+            this.Platform.Size = new System.Drawing.Size(157, 20);
+            this.Platform.TabIndex = 23;
+            // 
+            // Date
+            // 
+            this.Date.Location = new System.Drawing.Point(457, 174);
+            this.Date.Name = "Date";
+            this.Date.ReadOnly = true;
+            this.Date.Size = new System.Drawing.Size(157, 20);
+            this.Date.TabIndex = 24;
+            // 
+            // Hour
+            // 
+            this.Hour.Location = new System.Drawing.Point(457, 214);
+            this.Hour.Name = "Hour";
+            this.Hour.ReadOnly = true;
+            this.Hour.Size = new System.Drawing.Size(157, 20);
+            this.Hour.TabIndex = 25;
+            // 
+            // theorical
+            // 
+            this.theorical.Location = new System.Drawing.Point(301, 305);
+            this.theorical.Name = "theorical";
+            this.theorical.ReadOnly = true;
+            this.theorical.Size = new System.Drawing.Size(157, 20);
+            this.theorical.TabIndex = 26;
+            // 
+            // Entry
+            // 
+            this.Entry.Location = new System.Drawing.Point(301, 358);
+            this.Entry.Name = "Entry";
+            this.Entry.ReadOnly = true;
+            this.Entry.Size = new System.Drawing.Size(157, 20);
+            this.Entry.TabIndex = 27;
+            // 
+            // textdealings
+            // 
+            this.textdealings.AutoSize = true;
+            this.textdealings.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textdealings.Location = new System.Drawing.Point(298, 9);
+            this.textdealings.Name = "textdealings";
+            this.textdealings.Size = new System.Drawing.Size(109, 18);
+            this.textdealings.TabIndex = 28;
+            this.textdealings.Text = "Transacciones";
+            // 
+            // checkBoxMANUAL
+            // 
+            this.checkBoxMANUAL.AutoSize = true;
+            this.checkBoxMANUAL.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.checkBoxMANUAL.Location = new System.Drawing.Point(170, 419);
+            this.checkBoxMANUAL.Name = "checkBoxMANUAL";
+            this.checkBoxMANUAL.Size = new System.Drawing.Size(121, 22);
+            this.checkBoxMANUAL.TabIndex = 29;
+            this.checkBoxMANUAL.Text = "Modo Manual";
+            this.checkBoxMANUAL.UseVisualStyleBackColor = true;
+            // 
+            // DisplayDataEweighIN
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(676, 552);
+            this.Controls.Add(this.checkBoxMANUAL);
+            this.Controls.Add(this.textdealings);
+            this.Controls.Add(this.Entry);
+            this.Controls.Add(this.theorical);
+            this.Controls.Add(this.Hour);
+            this.Controls.Add(this.Date);
+            this.Controls.Add(this.Platform);
+            this.Controls.Add(this.Plate);
+            this.Controls.Add(this.IdentyCard);
+            this.Controls.Add(this.Driver);
+            this.Controls.Add(this.Company);
+            this.Controls.Add(this.NumRif);
+            this.Controls.Add(this.NumDocument);
+            this.Controls.Add(this.ticket);
+            this.Controls.Add(this.textentry);
+            this.Controls.Add(this.texttheoretical);
+            this.Controls.Add(this.texthour);
+            this.Controls.Add(this.textdate);
+            this.Controls.Add(this.textplatform);
+            this.Controls.Add(this.textplate);
+            this.Controls.Add(this.textIdentity_card);
+            this.Controls.Add(this.textdriver);
+            this.Controls.Add(this.textcompany);
+            this.Controls.Add(this.textRif);
+            this.Controls.Add(this.textDocument);
+            this.Controls.Add(this.textticket);
+            this.Controls.Add(this.buttonnext);
+            this.Controls.Add(this.buttonback);
+            this.Name = "DisplayDataEweighIN";
+            this.Text = "DisplaydataEweighIN";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonback;
+        private System.Windows.Forms.Button buttonnext;
+        private System.Windows.Forms.Label textticket;
+        private System.Windows.Forms.Label textDocument;
+        private System.Windows.Forms.Label textRif;
+        private System.Windows.Forms.Label textcompany;
+        private System.Windows.Forms.Label textdriver;
+        private System.Windows.Forms.Label textIdentity_card;
+        private System.Windows.Forms.Label textplate;
+        private System.Windows.Forms.Label textplatform;
+        private System.Windows.Forms.Label textdate;
+        private System.Windows.Forms.Label texthour;
+        private System.Windows.Forms.Label texttheoretical;
+        private System.Windows.Forms.Label textentry;
+        private System.Windows.Forms.TextBox ticket;
+        private System.Windows.Forms.TextBox NumDocument;
+        private System.Windows.Forms.TextBox NumRif;
+        private System.Windows.Forms.TextBox Company;
+        private System.Windows.Forms.TextBox Driver;
+        private System.Windows.Forms.TextBox IdentyCard;
+        private System.Windows.Forms.TextBox Plate;
+        private System.Windows.Forms.TextBox Platform;
+        private System.Windows.Forms.TextBox Date;
+        private System.Windows.Forms.TextBox Hour;
+        private System.Windows.Forms.TextBox theorical;
+        private System.Windows.Forms.TextBox Entry;
+        private System.Windows.Forms.Label textdealings;
+        private System.Windows.Forms.CheckBox checkBoxMANUAL;
+    }
+}
